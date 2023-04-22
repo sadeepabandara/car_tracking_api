62325b51217a06706646a9738e34805a55d1a4e9
Sadeepa Bandara <cobsccomp212p-055@student.nibm.lk>
2023-04-23 01:30:25 +0530

    Updated

--- index.js
+++ index.js
@@ -1,43 +1,74 @@
-//Dependencies
 const express = require("express");
-const app = express();
-app.use(express.json());
-//Handling Get request for / URI
+const PORT = process.env.PORT || 8090;
+let app = express();
+let fire = require("./fire");
+let cors = require("cors");
+let bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  res.send("Express App Running");
+  res.send(
+    "<h1>Firebase Cloud Firestore</h1><ul><li><p><b>GET /fetch</b></p></li><li><p><b>POST /add</b>  => {acceleration, humidity, location, pressure, rotation, temperature}</p></li></ul>"
+  );
 });
 
-//Handling Get server time
-app.get("/time", (req, res) => {
-  const dateObject = new Date();
-  let time = dateObject.getTime();
-  res.send("Server time " + time.toString());
+app.get("/fetch", (req, res) => {
+  const db = fire.firestore();
+  db.settings({
+    timestampsInSnapshots: true,
+  });
+  let wholeData = [];
+  db.collection("cars")
+    .orderBy(
+      "acceleration",
+      "humidity",
+      "location",
+      "pressure",
+      "rotation",
+      "temperature"
+    )
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((car) => {
+        // console.log(doc.id, '=>', doc.data());
+        // console.log(doc.data().name + doc.data().age);
+        // console.log(doc.data());
+        wholeData.push(car.data());
+      });
+      console.log(wholeData);
+      res.send(wholeData);
+    })
+    .catch((error) => {
+      console.log("Error!", error);
+    });
 });
 
-app.get("/sendValue", function (req, res) {
-  const sensorReading = req.query.temp;
-  if (sensorReading) {
-    //UpdateDB(sensorReading)
-    res.send("Sensor reading = " + sensorReading);
-  } else {
-    res.send("Temp query parameter is not set in request");
-  }
+app.post("/add", (req, res) => {
+  const db = fire.firestore();
+  db.settings({
+    timestampsInSnapshots: true,
+  });
+  db.collection("cars").add({
+    acceleration: req.body.acceleration,
+    humidity: req.body.humidity,
+    location: req.body.location,
+    pressure: req.body.pressure,
+    rotation: req.body.rotation,
+    temperature: req.body.temperature,
+  });
+  res.send({
+    acceleration: req.body.acceleration,
+    humidity: req.body.humidity,
+    location: req.body.location,
+    pressure: req.body.pressure,
+    rotation: req.body.rotation,
+    temperature: req.body.temperature,
+  });
 });
 
-app.post("/handleJSON", function (req, res) {
-  console.log(req.body);
-  const sensorReading = req.body.temp;
-  if (sensorReading) {
-    //UpdateDB(sensorReading)
-    res.send("Sensor 1 reading = " + sensorReading);
-  } else {
-    res.send("Temp JSON parameter is not set in request");
-  }
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
-
-//Deploying the listener
-const port = 8090;
-app.listen(process.env.PORT || port, () =>
-  console.log(`Express server listening on port
-${port}`)
-);
