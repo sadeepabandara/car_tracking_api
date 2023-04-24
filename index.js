d23f4b7f0017fdaf8500e4b5c0963b3e32000d76
Sadeepa Bandara <cobsccomp212p-055@student.nibm.lk>
2023-04-24 21:34:43 +0530

    v2.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 
 const admin = require("firebase-admin");
 const credentials = require("./key.json");
